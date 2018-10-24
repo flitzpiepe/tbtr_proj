0573c9ffef3d1c402a512a4ad181a0428c14a822
Thomas Schmidt <streen01@gmx.de>
2018-10-24 21:45:39 +0200

    header-only debugging
    To use it, the header must still be included by some source file to be
    usable in gdb. But at least the source.list is not needed to be changed
    anymore.

--- src/tbtr_debug.h
+++ src/tbtr_debug.h
@@ -17,10 +17,42 @@
 
 namespace tbtrdbg
 {
-	void chain(const Vehicle*);
-	template <typename T> void vehicle(const T*);
-	void all_trains();
-	void all_templates();
+	template<typename T>
+	void chain(const T* v)
+	{
+		if (!v) {
+			std::cout << std::endl;
+			return;
+		}
+		std::cout << "\t" << v->index << ":" << v->engine_type;
+		chain(v->Next());
+	}
+
+	template <typename T>
+	void vehicle(const T* v)
+	{
+		if (!v) return;
+		std::cout << "vehicle: " << v->index << ":" << v->type << std::endl;
+		chain(v);
+	}
+
+	void all_trains()
+	{
+		Train* t;
+		FOR_ALL_TRAINS(t)
+		{
+			vehicle(t);
+		}
+	}
+
+	void all_templates()
+	{
+		TemplateVehicle* tv;
+		FOR_ALL_TEMPLATES(tv)
+		{
+			vehicle(tv);
+		}
+	}
 }
 
 #endif
