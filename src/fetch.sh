9209e8e6b11a3733441beeaff6d86991db49d116
Thomas Schmidt <streen01@gmx.de>
2018-11-20 15:47:52 +0100

    todo update

--- src/fetch.sh
+++ src/fetch.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
 base=/home/streen/code/openttd
-tbtr=tbtr_new
+tbtr=tbtr
+env=$(dirname $0)/fetch.env
+if [[ -f $env ]]
+then
+	base=$(head -1 $env)
+	tbtr=$(tail -1 $env)
+fi
 log=$base/src/fetch.log
 remote=bb
-ret=1
+dosleep=2
+ret=0
 
-echo fetch base
+echo fetch base: $base
 cd $base
 while [[ $ret != 0 ]]
 do
-	printf ' .'
+	printf '.'
 	date >>$log
-	git fetch $remote >>$log
+	git fetch $remote >>$log 2>&1
 	ret=$?
-	[[ ret != 0 ]] && sleep 1
+	[[ ret != 0 ]] && sleep $dosleep
 done
 echo
 
-echo fetch tbtr
+echo fetch tbtr: $tbtr
+ret=1
 cd $tbtr
 while [[ $ret != 0 ]]
 do
-	printf ' .'
+	printf '.'
 	date >>$log
-	git fetch $remote >>$log
+	git fetch $remote >>$log 2>&1
 	ret=$?
-	[[ ret != 0 ]] && sleep 1
+	[[ ret != 0 ]] && sleep $dosleep
 done
 echo
