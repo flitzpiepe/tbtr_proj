9228ce636566e781c22ca5f8eadc8378df79cdd9
Thomas Schmidt <streen01@gmx.de>
2019-02-21 15:53:44 +0100

    todo update + added launcher

---
+++ tools/launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+openttd=/home/streen/code/openttd/tbtr_new/bin/openttd
+cfg=/home/streen/.openttd/openttd.cfg
+
+zoom=
+
+while getopts ':z:' opt
+do
+	case $opt in
+		z)
+			zoom=$OPTARG
+			;;
+		*)
+			echo unrecognized option, ignore
+			;;
+	esac
+done
+
+[[ $zoom != '' ]] && sed -i "s/^gui_zoom.*/gui_zoom = $zoom/" $cfg
+
+$openttd -s null -g tbtr.sav
